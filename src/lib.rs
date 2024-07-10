05809890f571ad57c6971537995a05c3f3e24aca
josibake <josibake@protonmail.com>
2024-07-10 17:11:32 +0200

    add skeleton for generating bindings
    add minimal code needed for generating _capnp.rs files from a schema.
    this allows other crates to import just the bindings without needing to
    worry about schema files / generating the bindings themselves / etc

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+pub mod chain_capnp {
+    include!(concat!(env!("OUT_DIR"), "/chain_capnp.rs"));
+}
+pub mod common_capnp {
+    include!(concat!(env!("OUT_DIR"), "/common_capnp.rs"));
+}
+pub mod echo_capnp {
+    include!(concat!(env!("OUT_DIR"), "/echo_capnp.rs"));
+}
+pub mod handler_capnp {
+    include!(concat!(env!("OUT_DIR"), "/handler_capnp.rs"));
+}
+pub mod init_capnp {
+    include!(concat!(env!("OUT_DIR"), "/init_capnp.rs"));
+}
+pub mod node_capnp {
+    include!(concat!(env!("OUT_DIR"), "/node_capnp.rs"));
+}
+pub mod proxy_capnp {
+    include!(concat!(env!("OUT_DIR"), "/proxy_capnp.rs"));
+}
+pub mod wallet_capnp {
+    include!(concat!(env!("OUT_DIR"), "/wallet_capnp.rs"));
+}
